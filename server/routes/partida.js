05fdda74b9a0388a5276c8df88972234b2f77d44
PaulinaArvizu <is707571@iteso.mx>
2020-04-27 17:12:42 -0500

    Endpoints para partida
    get, get:id, post, put y delete implementados

---
+++ server/routes/partida.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const Partida = require('../models/partida');
+router.get('/partidas', async (req, res) => {
+        let a = await Partida.getPartidas();
+        res.status(200).send(a);
+    })
+    .post('/partidas', async (req, res) => {
+        let newGame = req.body;
+        if (!newGame.jugador1 || !newGame.jugador2 || !newGame.album) {
+            res.status(400).send('Faltan atributos de la partida.');
+        } else {
+            let a = Partida.createPartida(newGame.jugador1, newGame.jugador2, newGame.album);
+            res.status(201).send(a);
+        }
+    })
+    .get('/partidas/:id', async (req, res) => {
+        let a = await Partida.getPartidaById(req.params.id);
+        res.status(200).send(a);
+    })
+    .put('/partidas/:id', async (req, res) => {
+        let newGame = req.body;
+        if (!newGame.jugador1 || !newGame.jugador2 || !newGame.album) {
+            res.status(400).send('Faltan atributos de la partida.');
+        } else {
+            let a = Partida.putPartida(req.params.id, newGame);
+            res.status(200).send(a);
+        }
+
+    })
+    .delete('/partidas/:id', async (req, res) => {
+        let a = Partida.deletePartida(req.params.id);
+        res.status(200).send(a);
+    })
+
+module.exports = router;
