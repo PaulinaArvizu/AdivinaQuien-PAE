9aa1b9c5a96af160f50620c4d2525e1c3b24ca4d
PaulinaArvizu <is707571@iteso.mx>
2020-04-27 17:01:45 -0500

    Correccion de endpoints
    No se obtiene el id del usuario, por lo que se quitó esa validacion.
    Correccion de manejo de parametros y atributos para cada metodo.

--- server/routes/album.js
+++ server/routes/album.js
@@ -1,57 +1,35 @@
 const router = require('express').Router();
-const album = require('../models/album')
-const user = require('../models/usuario')
+const Album = require('../models/album');
 router.get('/albums', async (req, res) => {
-        if (req.user == undefined) {
-            res.status(400).send('Sesion no iniciada');
-        } else {
-            let u = await user.getUserByEmail(req.user.email);
-            let a = [];
-            u.albumes.forEach(element => {
-                a.push(element);
-            });
-            return a;
-        }
+        let a = await Album.getAlbums();
+        res.status(200).send(a);
     })
     .post('/albums', async (req, res) => {
-        if (req.user == undefined) {
-            res.status(400).send('Sesion no iniciada');
+        let newAlbum = req.body;
+        if (!newAlbum.nombre || !newAlbum.fotos) {
+            res.status(400).send('Faltan atributos del album.');
         } else {
-            let newAlbum = req.body;
-            if (!newAlbum.nombre || !newAlbum.fotos) {
-                res.status(400).send('Faltan atributos del album')
-            } else {
-                user.addAlbum(req.user.email, newAlbum)
-                let a = album.createAlbum(newAlbum.nombre, newAlbum.fotos)
-                res.status(201).send(a)
-            }
+            let a = Album.createAlbum(newAlbum.nombre, newAlbum.fotos);
+            res.status(201).send(a);
         }
     })
     .get('/albums/:id', async (req, res) => {
-        if (req.user == undefined) {
-            res.status(400).send('Sesion no iniciada');
-        } else {
-            let a = await album.getAlbumById(req.body.uid)
-            res.status(200).send(a)
-        }
+        let a = await Album.getAlbumById(req.params.id);
+        res.status(200).send(a);
     })
     .put('/albums/:id', async (req, res) => {
-        if (req.user == undefined) {
-            res.status(400).send('Sesion no iniciada');
+        let newAlbum = req.body;
+        if (!newAlbum.nombre || !newAlbum.fotos) {
+            res.status(400).send('Faltan atributos del album.');
         } else {
-            let changeAlbum = req.body;
-            if (!changeAlbum.nombre || !changeAlbum.fotos) {
-                res.status(400).send('Faltan atributos del album')
-            } else {
-                album.putAlbum(req.params.id, changeAlbum)
-            }
+            let a = Album.putAlbum(req.params.id, newAlbum);
+            res.status(200).send(a);
         }
+
     })
     .delete('/albums/:id', async (req, res) => {
-        if (req.user == undefined) {
-            res.status(400).send('Sesion no iniciada');
-        } else {
-            
-        }
+        let a = Album.deleteAlbum(req.params.id);
+        res.status(200).send(a);
     })
+
 module.exports = router;
