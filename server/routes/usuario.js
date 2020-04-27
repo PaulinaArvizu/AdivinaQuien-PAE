c1840b010cd300d50ae52a3248922e664ffeb4d8
PaulinaArvizu <is707571@iteso.mx>
2020-04-27 17:19:51 -0500

    Correccion de metodos
    Correccion del metodo post con validacion de atributos del body.
    Correccion de sintaxis en metodo put

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,26 +1,30 @@
 const router = require('express').Router();
-const user = require('../models/usuario')
-router.post('/users',async (req, res) => {
-        let newUser = await user.createUser(req.body.email,req.body.password,req.body.nombre)
-        if(newUser != undefined){
+const User = require('../models/usuario')
+router.post('/users', async (req, res) => {
+        if (!req.body.email || !req.body.password || !req.body.nombre) {
+            res.status(400).send('Faltan datos.');
+            return;
+        }
+        let newUser = await User.createUser(req.body.email, req.body.password, req.body.nombre)
+        if (newUser != undefined) {
             res.status(201).send(a)
-        }else{
-            res.status(400).send('No se pudo agregar')
+        } else {
+            res.status(400).send('Faltan datos.');
         }
     })
-    .get('/users', async(req,res)=>{
-        let u = await user.getUsers();
+    .get('/users', async (req, res) => {
+        let u = await User.getUsers();
         res.status(200).send(u);
     })
-    .get('/users/:email', async(req,res)=>{
-        let u = await user.getUserByEmail(req.params.email)
+    .get('/users/:email', async (req, res) => {
+        let u = await User.getUserByEmail(req.params.email)
         res.status(200).send(u);
     })
-    .put('/users/:email', async(req,res)=> {
-        let u = await user.putUser(req.params.email, req.params.body)
-        if(u){
+    .put('/users/:email', async (req, res) => {
+        let u = await User.putUser(req.params.email, req.body)
+        if (u) {
             res.status(200).send(u);
-        }else{
+        } else {
             res.status(400).send("Usuario no existe");
         }
     })
