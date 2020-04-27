7b7d8cb2592e80a893a54d9afb51857f3e7f8dd1
PaulinaArvizu <is707571@iteso.mx>
2020-04-27 17:00:00 -0500

    Correccion de funciones
    Se eliminó addPhoto, addAlbum, addFriend

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -4,7 +4,7 @@ class Usuario extends DB {
     constructor() {
         super();
         this.schema = new mongoose.Schema({
-            email:{
+            email: {
                 type: String,
                 unique: true
             },
@@ -39,66 +39,44 @@ class Usuario extends DB {
         });
         this._model = mongoose.model('Usuario', this.schema);
     }
-    
-    async getUsers(){
+
+    async getUsers() {
         return await super.query({})
     }
 
     async getUserByEmail(email, projection = "", options = {}) {
-        return await super.queryOne({'email':email},projection,options);
+        return await super.queryOne({
+            'email': email
+        }, projection, options);
     }
     async exists(conditions) {
-        let doc = await super.exists(conditions);
-        return doc;
+        return await super.exists(conditions);
     }
     async createUser(email, password, nombre) {
-        if(!email || !password || !nombre){
+        if (!email || !password || !nombre) {
             return undefined;
         }
-        return super.add({  'email':email,
-                            'password':password,
-                            'nombre':nombre,
-                            'fotoPerfil':'',
-                            'historialPartidas':[],
-                            'albumes':[],
-                            'fotos':[],
-                            'amigos':[]
-                        })
+        return super.add({
+            'email': email,
+            'password': password,
+            'nombre': nombre,
+            'fotoPerfil': '',
+            'historialPartidas': [],
+            'albumes': [],
+            'fotos': [],
+            'amigos': []
+        })
     }
 
-    async addPhoto(email, photoId) {
-        let u  = await this.getUserByEmail(email);
-        u.fotos.push(photoId);
-        return await super.update({'email':email}, u)
-    }
-
-    async addAlbum(email, albumId) {
-        let u  = await this.getUserByEmail(email);
-        u.albumes.push(photoId);
-        return await super.update({'email':email}, u)
-    }
-
-    async addFriend(email, friendEmail){
-        let u  = await this.getUserByEmail(email);
-        u.amigos.push(friendEmail)
-        return await super.update({'email':email}, u)
-    }
-    async deleteFriend(email, friendEmail){
-        let u  = await this.getUserByEmail(email);
-        let i = u.amigos.findIndex((e) => e == friendEmail)
-        u.amigos.splice(i,1)
-        return await super.update({'email':email}, u)
-    }
-
-    async putUser(email, user){
-        if(email != user.email) return;
-        let exists = await super.exists({'email': email})
-        if(exists){
-            let u = await this.update({'email': email}, user)
-            return u;
+    async putUser(email, user) {
+        if (email != user.email) return;
+        let exists = await super.exists({
+            'email': email
+        })
+        if (exists) {
+            return await this.update({'email': email}, user);
         }
-
     }
 }
-let usuario= new Usuario();
+let usuario = new Usuario();
 module.exports = usuario;
