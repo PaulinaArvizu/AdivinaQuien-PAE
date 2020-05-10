85e2ce01fb06a3657904979f9c998204fcad4617
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-05-10 00:19:59 -0500

    routing parcialmente configurado
    Se agregan los componentes y las rutas tentativas, falta agregar la funcionalidad de los servicios. Proximo patch tendrá agregada la funcionalidad mencionada para login y los servicios de auth.

--- adivinaQuien/src/app/app-routing.module.ts
+++ adivinaQuien/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { GameComponent } from './game/game.component';
+import { ProfileComponent } from './profile/profile.component';
+import { componentFactoryName } from '@angular/compiler';
+import { AuthGuardService } from './auth-guard.service';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile/:id', component: ProfileComponent, canActivate: [AuthGuardService]},
+  {path: 'google/redirect', component: LoginComponent},
+  {path: 'game/:id', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
