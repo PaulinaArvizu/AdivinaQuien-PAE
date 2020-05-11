2c50ce306b7167646543ffd085c36f6118aea45e
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-05-11 03:17:45 -0500

    Funcionalidad casi completa
    Falta implementar funciones de login y registro usando datos externos. Validar existencia de token para hacer redirect a profile.

--- adivinaQuien/src/app/login/login.component.ts
+++ adivinaQuien/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+     this.route.queryParams.subscribe((params) => {
+       if(params.code){
+         this.authService.googleLogin(params).subscribe((data) => {
+           if(this.authService.isLoggedIn()){
+              this.router.navigateByUrl('profile');
+           }
+         })
+       }
+     });
+  }
+
+  //validar existencia token para redirect a profile. Usar funcion de linea 18 a 22.
+
+  //Registro y login de usuarios sin passport, preguntar al profe.
+
+  submit(form: NgForm){
+    console.log(form.value.nombre, form.value.password);
+    this.authService.login(form.value.nombre, form.value.password)
+        .subscribe((data)=> console.log(data), (err)=> console.log(err))
   }
 
 }
