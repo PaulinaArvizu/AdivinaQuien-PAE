94f377b8047258b46b9ae1a0a61d9229a0f8f0c0
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-05-13 03:39:50 -0500

    Update passportGoogle.js

--- adivinaQuien/server/routes/passportGoogle.js
+++ adivinaQuien/server/routes/passportGoogle.js
@@ -1,40 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const googleConfig = require('../googleConfig');
-const User = require('../models/usuario');
+const users = require('../users.json');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-
 passport.use(new GoogleStrategy({
-    clientID: googleConfig.clientID,
     clientSecret: googleConfig.clientSecret,
     callbackURL: googleConfig.callbackURL //Se necesita poner herokuapp una vez implementado. Por el momento sera localhost:3000/google/redirect
 }, function(accessToken, refreshToken, profile, done){
-    //console.log('este es profile');
-    //console.log(profile);
+    console.log('este es profile');
+    console.log(profile);
     if(profile == null){
         done(null, false, {error: "No fue posible autenticarse"})
         return;
     }
 
+    let newUser= {
+        email: profile._json.email,
+        info: profile._json
+    }
 
-    let currUser = User.getUserByEmail(profile._json.email);
-    console.log('Usuario encontrado: ' + currUser.email);
-    let found = !(currUser.email == undefined);
-    console.log(found);
+    let findUser = users.find(u => u.email == newUser.email); // GET User con email de la API
+    
 
-    if(found){//Sign in
-        done(null, currUser);
+    if(findUser){//Sign in
+        done(null, findUser);
         return;
     }      
     else
     {//Registro
-        console.log('Usuario a crear');
-        let newUser = User.createUser(profile._json.email, random_password_generate(8, 16), profile._json.name);
-        console.log(newUser.email);
+        users.push(newUser);
+        fs.writeFileSync('users.json', JSON.stringify(users)); //POST de la API
+        
         done(null, newUser);
-        return;
+
     }
 
 
@@ -53,26 +53,3 @@ function random_password_generate(max,min)
 function googleLogin(req, res){
 
     console.log("Entrando a googleLogin");
-    passport.authenticate('google', (err, user, info) => {
-        console.log("JohnnyTest");
-        console.log(user);
-        if (user) {
-            let token = jwt.sign({
-                nombre: user.info.name,
-                email: user.email,
-                fotoPerfil: user.info.picture
-            }, 'secret', {
-                expiresIn: '1h' 
-            });
-            console.log(token);
-            res.send({
-                token
-            })
-        } else {
-            res.status(401).send(info)
-        }
-    })(req, res)
-
-}
-
-module.exports = {googleLogin};
