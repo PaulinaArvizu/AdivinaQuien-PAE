358848ff6d245483cfe3dc8a85bf2457c40a7a11
PaulinaArvizu <is707571@iteso.mx>
2020-05-13 16:46:39 -0500

    Agregar variables, funciones y validaciones
    Validacion de que el usuario tenga algunos atributos no vacios para hacer los GETs de esas variables.
    Correccion de dedo de "albums" a "albumes"
    Actualizar funcion de deleteGame
    Agregar funcionalidad a crearJuego y nuevoAlbum

--- adivinaQuien/src/app/profile/profile.component.ts
+++ adivinaQuien/src/app/profile/profile.component.ts
@@ -13,14 +13,14 @@ export class ProfileComponent implements OnInit {
 
 	//declarar datos
 	user;
-	allUsers;
-	albums;
-	fotos;
-	juegos;
-	juegosPendientes;
-	amigos;
+	allUsers = [];
+	albums = [];
+	fotos = [];
+	juegos = [];
+	amigos = [];
 	resBusqueda = [];
 	newGame;
+	newAlbum;
 
 	//Generar servicio que me traiga los datos relacionados al usuario.
 	constructor(private usersService: UsersService, private authService: AuthService) { } //importar servicio
@@ -28,27 +28,24 @@ export class ProfileComponent implements OnInit {
 	ngOnInit(): void {
 		// console.log(this.authService.getTokenData());
 		let email = this.authService.getTokenData().email;
+		console.log(email);
 		this.user = this.usersService.getOneUser(email);
+		console.log(this.user);
 		this.allUsers = this.usersService.getAllUsers();
-		this.albums = this.user.albums.map(a => this.usersService.getOneAlbum(a));
-		this.fotos = this.user.fotos.map(f => this.usersService.getOneFoto(f));
-		this.amigos = this.allUsers.filter(u => this.user.amigos.includes(u.email));
+		if (this.user.albumes) this.albums = this.user.albumes.map(a => this.usersService.getOneAlbum(a));
+		if (this.user.fotos) this.fotos = this.user.fotos.map(f => this.usersService.getOneFoto(f));
+		if (this.user.amigos) this.amigos = this.allUsers.filter(u => this.user.amigos.includes(u.email));
 
-		this.juegos = this.user.historialPartidas.map(g => this.usersService.getOneGame(g));
-		this.juegos.forEach(g => {
-			if (g.jugador1 != this.user.email) {
-				let temp = g.jugador1;
-				g.jugador1 = this.user.email;
-				g.jugador2 = temp;
-			}
-		})
-
-		this.juegosPendientes = this.juegos.filter(g => !g.status);
-		this.juegosPendientes.forEach(g => {
-			g.jugador2 = this.amigos.find(a => a.email == g.jugador2);
-		})
-
-
+		if (this.user.historialPartidas) {
+			this.juegos = this.user.historialPartidas.map(g => this.usersService.getOneGame(g));
+			this.juegos.forEach(g => {
+				if (g.jugador1 != this.user.email) {
+					let temp = g.jugador1;
+					g.jugador1 = this.user.email;
+					g.jugador2 = temp;
+				}
+			})
+		}
 	}
 
 	//funciones
@@ -82,7 +79,7 @@ export class ProfileComponent implements OnInit {
 		} else return;
 
 		//actualizar variables de ablums y fotos
-		this.albums = this.user.albums.map(a => this.usersService.getOneAlbum(a));
+		this.albums = this.user.albumes.map(a => this.usersService.getOneAlbum(a));
 		this.fotos = this.user.fotos.map(f => this.usersService.getOneFoto(f));
 
 		//eliminar la foto
@@ -95,7 +92,7 @@ export class ProfileComponent implements OnInit {
 
 	deleteAlbum(id) {
 		//eliminar de la lista del usuario
-		let index = this.user.albums.findIndex(a => a == id)
+		let index = this.user.albumes.findIndex(a => a == id)
 		if (index >= 0) {
 			this.user.fotos.splice(index, 1);
 			this.usersService.updateUser(this.user);
@@ -140,18 +137,57 @@ export class ProfileComponent implements OnInit {
 			this.usersService.updateUser(this.user);
 		} else return
 
-		//eliminar de la lista del usuario que envió la invitacion
-		let user2 = this.allUsers.find(u => u.email);
-		if (user2) {
-			user2.historialPartidas.findIndex(j => j == id);
-			if (index >= 0) {
-				user2.historialPartidas.splice(index, 1);
-				this.usersService.updateUser(user2);
+		//buscar el juego
+		let game = this.juegos.find(j => j._id == id);
+		if (game) {
+			this.usersService.deleteGame(id);
+			//eliminar de la lista del usuario que envió la invitacion
+			let user2 = this.allUsers.find(u => u.email == game.jugador2);
+			if (user2) {
+				user2.historialPartidas.findIndex(j => j == id);
+				if (index >= 0) {
+					user2.historialPartidas.splice(index, 1);
+					this.usersService.updateUser(user2);
+					this.allUsers = this.usersService.getAllUsers();
+
+					//actualizar las listas de juegos
+					this.juegos = this.user.historialPartidas.map(g => this.usersService.getOneGame(g));
+					this.juegos.forEach(g => {
+						if (g.jugador1 != this.user.email) {
+							let temp = g.jugador1;
+							g.jugador1 = this.user.email;
+							g.jugador2 = temp;
+						}
+					})
+				}
 			}
 		}
 	}
 
 	crearJuego(newGame) {
+		// crear juego
+		if (!newGame.jugador1 || !newGame.jugador2 || !newGame.album) return;
+		let game = this.usersService.newGame(newGame);
+		if (game) {
+			this.user.historialPartidas.push(newGame._id);
+			let user2 = this.allUsers.find(u => u.email == newGame.jugador2);
+			if (user2) {
+				user2.historialPartidas.push(newGame._id);
+				this.usersService.updateUser(user2);
+			}
+		}
+	}
 
+	nuevoAlbum(newAlbum) {
+		if (newAlbum.fotos.length <= 0 || !newAlbum.nombre) return;
+		let album = this.usersService.newGame(newAlbum);
+		if (newAlbum) {
+			this.user.historialPartidas.push(newAlbum._id);
+			let user2 = this.allUsers.find(u => u.email == newAlbum.jugador2);
+			if (user2) {
+				user2.historialPartidas.push(newAlbum._id);
+				this.usersService.updateUser(user2);
+			}
+		}
 	}
 }
