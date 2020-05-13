f65c6fef60980350443b23a66edf6796c3fda16f
PaulinaArvizu <is707571@iteso.mx>
2020-05-13 00:35:39 -0500

    Habilitar modals

--- adivinaQuien/src/app/profile/profile.component.ts
+++ adivinaQuien/src/app/profile/profile.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  
-  //declarar datos
 
-  
-  //Generar servicio que me traiga los datos relacionados al usuario.
-  constructor() { } //importar servicio
+	//declarar datos
 
-  ngOnInit(): void { 
-  }
 
-  //funciones
+	//Generar servicio que me traiga los datos relacionados al usuario.
+	constructor() { } //importar servicio
 
+	ngOnInit(): void {
+	}
+
+	//funciones
+	openNewGameModal() {
+		$('#newGameModal').modal('show');
+	}
+
+	openNewImgModal() {
+		$('#newImgModal').modal('show');
+	}
+
+	openNewAlbumModal() {
+		$('#newAlbumModal').modal('show');
+	}
+
+	openEditAlbumModal() {
+		$('#editAlbumModal').modal('show');
+	}
+
+	openEditUserModal() {
+		$('#editUserModal').modal('show');
+	}
 
 }
