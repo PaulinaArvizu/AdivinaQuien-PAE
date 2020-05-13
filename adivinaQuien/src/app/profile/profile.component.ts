13d6123142a33df50cc26f45f7997c8bf6982e67
PaulinaArvizu <is707571@iteso.mx>
2020-05-13 06:14:53 -0500

    Agregar gets de variables en ngOnInit
    Se utilizan las funciones del servicio para hacer request a la base de datos

--- adivinaQuien/src/app/profile/profile.component.ts
+++ adivinaQuien/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { AuthService } from '../auth.service';
 
-declare var $:any;
+declare var $: any;
 
 @Component({
 	selector: 'app-profile',
@@ -10,12 +12,31 @@ declare var $:any;
 export class ProfileComponent implements OnInit {
 
 	//declarar datos
-
+	user;
+	allUsers;
+	albums;
+	fotos;
+	juegos;
 
 	//Generar servicio que me traiga los datos relacionados al usuario.
-	constructor() { } //importar servicio
+	constructor(private usersService: UsersService, private authService: AuthService) { } //importar servicio
 
 	ngOnInit(): void {
+		// console.log(this.authService.getTokenData());
+		let email = this.authService.getTokenData().email;
+		this.user = this.usersService.getOneUser(email);
+		this.allUsers = this.usersService.getAllUsers();
+		this.albums = this.user.albums.map(a => this.usersService.getOneAlbum(a));
+		this.fotos = this.user.fotos.map(f => this.usersService.getOneFoto(f));
+		this.juegos = this.user.historialPartidas.map(g => this.usersService.getOneGame(g));
+		
+		this.juegos.forEach(g => {
+			if(g.jugador1 != this.user.email) {
+				let temp = g.jugador1;
+				g.jugador1 = this.user.email;
+				g.jugador2 = temp;
+			}
+		})
 	}
 
 	//funciones
@@ -31,7 +52,7 @@ export class ProfileComponent implements OnInit {
 		$('#newAlbumModal').modal('show');
 	}
 
-	openEditAlbumModal() {
+	openEditAlbumModal(id) {
 		$('#editAlbumModal').modal('show');
 	}
 
@@ -39,4 +60,11 @@ export class ProfileComponent implements OnInit {
 		$('#editUserModal').modal('show');
 	}
 
+	deleteFoto(id) {
+		
+	}
+
+	deleteAlbum(id) {
+		
+	}
 }
