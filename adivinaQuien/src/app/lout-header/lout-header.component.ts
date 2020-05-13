f9e3d2b1c531767fac8c6bdeb874b7cb46db9779
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-05-13 01:48:38 -0500

    Funcionalidad de logout

--- adivinaQuien/src/app/lout-header/lout-header.component.ts
+++ adivinaQuien/src/app/lout-header/lout-header.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-lout-header',
   templateUrl: './lout-header.component.html',
-  styles: [
-  ]
+  styleUrls: ['./lout-header.component.scss']
 })
 export class LoutHeaderComponent implements OnInit {
 
-  constructor() { }
+  logueado = false;
+  
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.logueado.subscribe((val) => 
+      this.logueado = val
+      );
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
