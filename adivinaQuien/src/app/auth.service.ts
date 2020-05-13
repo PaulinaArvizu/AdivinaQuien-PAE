801410790059a04d368a64f19285d073ff69760b
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-05-13 09:05:07 -0500

    Funcionalidad de login local añadida

--- adivinaQuien/src/app/auth.service.ts
+++ adivinaQuien/src/app/auth.service.ts
@@ -27,11 +27,10 @@ export class AuthService {
 
   public isLoggedIn ():boolean {
     const tokenData = this.getTokenData();
-    // console.log(tokenData);
-
+    console.log(tokenData);
     if(tokenData) {
       let resp = tokenData.exp > Date.now() /1000;
-      // console.log(this.logueado);
+      console.log(this.logueado);
       this.logueado.next(true);
       return resp;
     }else{
@@ -40,31 +39,35 @@ export class AuthService {
     }
   }
 
-  public login(email:string, pass: string): Observable<any> {
+  public login(email: string, password: string): Observable<any> {
+    console.log("entra a login");
+    console.log(email, password);
     return this.http
-                    .post('/api/login', {email, pass})
+                    .post('/api/login', {email, password})
                     .pipe(
                       map((data:any)=> {
                         if(data.token){
+                          console.log('guardando token');
                           this.saveToken(data.token);
                           this.isLoggedIn();
                         }
                         return data;
                       })
-                    )
+                    );
   }
+
+  
 
   public logout() {
     this.token='';
     window.localStorage.removeItem('token');
     this.router.navigateByUrl('/');
     this.logueado.next(false);
-
   }
 
   public getTokenData () {
     let payload;
-    let strgToken = localStorage.getItem('token');
+    this.token = localStorage.getItem('token');
     if(this.token){
       payload = this.token.split('.')[1]; //obtiene el payload splieando la segunda parte del token
       payload = window.atob(payload); //obtiene la informacion embebida en el payload
