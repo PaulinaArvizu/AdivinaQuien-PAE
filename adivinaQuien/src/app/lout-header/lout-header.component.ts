a10381d1d688a667904fc11ff44d29d8d4cb3598
PaulinaArvizu <is707571@iteso.mx>
2020-05-13 01:42:07 -0500

    crear lout-header component

---
+++ adivinaQuien/src/app/lout-header/lout-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lout-header',
+  templateUrl: './lout-header.component.html',
+  styles: [
+  ]
+})
+export class LoutHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
