3175c6f3d6de173ec622720645507328a6154406
Alderbane <51372365+Alderbane@users.noreply.github.com>
2020-05-16 21:54:29 -0500

    avances juego
    Juego tiene imagen asignada, socket del juego

--- adivinaQuien/src/app/game/game.component.ts
+++ adivinaQuien/src/app/game/game.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocketIoService } from '../socket-io.service';
 import { Subscription } from 'rxjs';
+import { UsersService } from '../users.service';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
+
   gameStatus = {
     terminada: true,
     pendiente: false
@@ -15,52 +21,51 @@ export class GameComponent implements OnInit {
   msgChat: object[] = []
   msgSend = '';
   jugador = {
-    correo: "",
+    email: "",
     password: "",
-    nombre: "",
+    nombre: "Yo",
     fotoPerfil: "",
     historialPartidas: "",
     albumes: [],
     fotos: []
-};
-  myImg: string;
-  selectedImg: string;
+  };
+  myImg = {
+    _id: "",
+    url: "",
+    name: ""
+  };
+  selectedImg = {
+    _id: "",
+    url: "",
+    name: ""
+  };
+  album: {
+    _id: "",
+    url: "",
+    name: ""
+  }[] = [];
   game = {
-    id: 0,
-    Jugador1: {
-      correo: "",
-      password: "",
-      nombre: "",
-      fotoPerfil: "",
-      historialPartidas: "",
-      albumes: [],
-      fotos: []
-    },
-    Jugador2: {
-      correo: "",
-      password: "",
-      nombre: "",
-      fotoPerfil: "",
-      historialPartidas: "",
-      albumes: [],
-      fotos: []
-    },
+    _id: "",
+    Jugador1: "",
+    Jugador2: "",
     ganador: null,
     status: this.gameStatus.pendiente,
-    album: []
+    album: ""
   };
   guessVeredict: boolean = false;
   myTurn: boolean;
   contrincante = {
-    correo: "",
+    email: "",
     password: "",
-    nombre: "",
+    nombre: "El otro",
     fotoPerfil: "",
     historialPartidas: "",
     albumes: [],
     fotos: []
   };
-  constructor(private socketIOservice: SocketIoService) { }
+
+
+  constructor(private socketIOservice: SocketIoService, private userService: UsersService, private auth: AuthService, private route: ActivatedRoute) { }
   private subDatos: Subscription;
   private subGuess: Subscription;
   private subPregunta: Subscription;
@@ -69,46 +74,61 @@ export class GameComponent implements OnInit {
   private subPerder: Subscription;
   ngOnInit(): void {
     this.subDatos = this.socketIOservice.recibirDatosJuego().subscribe((datos) => {
+      console.log('recibe verga');
+      console.log(datos);
       this.game = datos;
+      this.myTurn = this.jugador.email === this.game.Jugador2;
+      console.log(this.myTurn);
+      console.log(this.game.album);
+      let album2 = this.userService.getOneAlbum(this.game.album)
+      console.log(album2);
+      this.album =  album2.fotos.map(e => this.userService.getOneFoto(e))
+      console.log(this.album);
 
-      this.myTurn = this.jugador.correo == this.game.Jugador2.correo;
+      this.myImg = this.album[parseInt(Math.random() * this.album.length)];
+      if (this.jugador.email === this.game.Jugador1) {
+        this.contrincante = this.userService.getOneUser(this.game.Jugador2)
+      } else {
+        this.contrincante = this.userService.getOneUser(this.game.Jugador1)
+      }
 
     });
     this.subGuess = this.socketIOservice.recibirGuess().subscribe((guess) => {
-      console.log('recibiendo guess')
       this.myTurn = true
-      let win = this.myImg !=guess;
-      if(win){
-        console.log('Victoria')
-      } else {
-        console.log('Derrota');
+      let adivino = this.myImg._id == guess._id;
+      if (adivino) {
+        alert('perdiste')
+        this.socketIOservice.enviarVeredicto({ gameId: this.game._id, userEmail: this.contrincante.email, adivino })
       }
-      this.socketIOservice.enviarVeredicto({gameId:this.game.id, userEmail: this.jugador.correo, win})
+
     });
     this.subPregunta = this.socketIOservice.recibirPregunta().subscribe((pregunta: string) => {
-      
       this.msgChat.push({
         recibida: true,
         pregunta: pregunta
       })
-      
     });
-    this.subRespuesta = this.socketIOservice.recibirRespuesta().subscribe((resp)=>{
-      console.log(resp);
-      if(resp == 'yes'){
-
+    this.subRespuesta = this.socketIOservice.recibirRespuesta().subscribe((resp) => {
+      
+      if (resp == 'yes') {
+        $(`[name="yes"]`).last().removeClass("btn-outline-primary");
+        $(`[name="yes"]`).last().addClass("btn-primary");
       } else {
-
+        $(`[name="no"]`).last().removeClass("btn-outline-danger");
+        $(`[name="no"]`).last().addClass("btn-danger");
       }
     });
     this.subPerder = this.socketIOservice.perderJuego().subscribe(() => {
       console.log('perder')
-      // this.game.
     });
-    this.subGanar = this.socketIOservice.ganarJuego().subscribe(()=>{
+    this.subGanar = this.socketIOservice.ganarJuego().subscribe(() => {
       console.log('ganar');
     });
-    this.socketIOservice.entrarAlJuego(0);
+    // this.jugador = this.userService.getOneUser(this.auth.getTokenData().email)
+    console.log(this.route.snapshot.paramMap.get('id'));
+    this.socketIOservice.entrarAlJuego(this.route.snapshot.paramMap.get('id'));
+    this.jugador = this.userService.getOneUser(this.auth.getTokenData().email)
+    
   }
 
   ngOnDestroy() {
@@ -121,31 +141,31 @@ export class GameComponent implements OnInit {
   }
 
   enviarMensaje(event) {
-    if (event.keyCode != 13) return;
-    this.socketIOservice.enviarPregunta({gameId:this.game.id,mensaje:this.msgSend})
+    if (event.keyCode != 13 || !this.myTurn) return;
+    this.myTurn = false;
+    this.socketIOservice.enviarPregunta({ gameId: this.game._id, mensaje: this.msgSend.trim() })
     // console.log(this.msgSend)
     this.msgChat.push({
       recibida: false,
-      pregunta: this.msgSend
+      pregunta: this.msgSend.trim()
     })
     this.msgSend = ''
   }
-  enviarGuess(){
-    // if(!this.myTurn) return;
-    console.log('Mandando guess')
+  enviarGuess() {
+    if (!this.myTurn) return;
     this.myTurn = false;
-    let a = {gameId: this.game.id, img:this.selectedImg}
+    let a = { gameId: this.game._id, img: this.selectedImg }
     this.socketIOservice.enviarGuess(a);
   }
 
-  enviarRespuesta(resp: string){
-    this.socketIOservice.enviarRespuesta({gameId:this.game.id,respuesta:resp})
+  enviarRespuesta(resp: string) {
+    this.socketIOservice.enviarRespuesta({ gameId: this.game._id, respuesta: resp })
   }
 
-  seleccionarImagen(imgUrl: string){
-    this.selectedImg = imgUrl;
+  seleccionarImagen(img: { _id: "", url: "", name: "" }) {
+    this.selectedImg = img;
   }
-  disable(event){
-    console.log(event);
+  disable(event) {
+    $('')
   }
 }
